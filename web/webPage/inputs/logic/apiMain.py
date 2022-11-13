1ec1b714b10e5ee12b08f84c165d9285bd17b232
Jessica <j.roblesm@uniandes.edu.co>
2022-11-13 17:00:16 -0500

    con API

---
+++ web/webPage/inputs/logic/apiMain.py
@@ -0,0 +1,33 @@
+from typing import Optional
+
+from fastapi import FastAPI
+from joblib import load, dump
+import pandas as pd
+
+from .DataModel import DataModel
+from .DataModel import JsonInput
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+   return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Optional[str] = None):
+   return {"item_id": item_id, "q": q}
+
+@app.post("/predict")
+def make_predictions(dataModel: JsonInput):
+      petition_dict = dataModel
+      results = proccess(petition_dict)
+      return {"result": results}
+
+def proccess(petition):
+    print(petition)
+    model = load("modelo2.joblib")
+    result = model.predict([petition])
+    print(result[0])
+    return result[0]
--- web/webPage/inputs/logic/logic_inputs.py
+++ web/webPage/inputs/logic/logic_inputs.py
@@ -1,5 +1,6 @@
 from ..models import Input
 from joblib import load
+from .apiMain import make_predictions
 
 def get_inputs():
     queryset = Input.objects.all()
@@ -8,11 +9,12 @@ def get_inputs():
 def create_inputs(form):
     inputs = form.save(commit=False)
     text = inputs.text
-    result = predict(text)
+    result = make_predictions(text)["result"]
     input = Input(text=text, result=result)
     input.save()
     return ()
 
+#Funcion para hacerlo desde la logica
 def predict(text):
     model = load("modelo2.joblib")
     result = model.predict([text])
